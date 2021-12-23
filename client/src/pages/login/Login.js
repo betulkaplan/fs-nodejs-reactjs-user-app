9f4a61fe779d17987283728ac29958e50f235d87
Betul <betulkaplan7@gmail.com>
2021-12-23 21:47:40 +0100

    feat: add login function

---
+++ client/src/pages/login/Login.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Form, Input, Button, Checkbox, Divider } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import style from './Login.module.css';
+import { ErrorNotification, SuccessNotification } from "../../helpers/Notifications";
+
+const Login = () => {
+    const onFinish = async (values) => {
+        console.log('Received values of form: ', values);
+        try {
+            const res = await fetch(`http://localhost:5000/auth/login/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(values),
+            });
+            const response = await res.json();
+            console.log(res.ok);
+            console.log(response.message);
+            res.ok ? SuccessNotification({ description: response.message }) : ErrorNotification({ description: response.message });
+            // SuccessNotification({ description: response.message });
+            // navigate("/")
+        } catch (error) {
+            ErrorNotification({ description: error.message })
+        }
+
+    };
+
+    return (
+        <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            className={style.form}
+        >
+            <h2>Login</h2>
+            <Divider />
+            <Form.Item
+                name="email"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Username!',
+                    },
+                ]}
+
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Password!',
+                    },
+                ]}
+            >
+                <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
+            <Form.Item>
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <a className="login-form-forgot" href="">
+                    Forgot password
+                </a>
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                </Button><br />
+                Or <br /> <a href="">register now!</a>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default Login;
--- client/src/router/AppRouter.js
+++ client/src/router/AppRouter.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "../pages/details/Details";
 import Home from "../pages/home/Home";
+import Register from "../pages/register/Register";
+import Login from "../pages/login/Login";
 import Add from "../pages/user/Add";
 
 const AppRouter = () => {
@@ -10,6 +12,8 @@ const AppRouter = () => {
         <Route exact path="/" element={<Home />}></Route>
         <Route path="/detail/:id" element={<Details />}></Route>
         <Route path="/add" element={<Add />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
