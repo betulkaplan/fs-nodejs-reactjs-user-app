e7198c6e1026e9701a59dd923a08d3c2c4704419
Betul <betulkaplan7@gmail.com>
2021-12-27 21:44:07 +0100

    add image display link

--- server.js
+++ server.js
@@ -73,8 +73,47 @@ const storage = new GridFsStorage({
 const upload = multer({ storage });
 
 app.post('/upload', upload.single('file'), (req, res) => {
-  res.json({ file: req.file });
+  // res.json({ file: req.file });
 })
+
+//display all files
+app.get("/files", (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    //check if files
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: "No files exist",
+      });
+    }
+    return res.json(files);
+  });
+});
+
+app.get("/image/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    //check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No files exist",
+      });
+    }
+    //check if image
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      //read output to browser
+      console.log('breakpoint')
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: "Not an image",
+      });
+    }
+  });
+});
+
+
+
+
 
 
 app.use("/users", userRouter);
