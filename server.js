d1bb8936dafd8d9f3ecfe9b8ff1b5eb0ee0e4cd6
Betul <betulkaplan7@gmail.com>
2021-12-27 14:12:13 +0100

    add image upload to mongodb

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const userRouter = require("./routes/users");
 const homeRouter = require("./routes/home");
 const authRouter = require("./routes/auth");
 const productRouter = require("./routes/product");
+
+//traversy media
+const path = require("path");
+const crypto = require("crypto");
+const multer = require("multer");
+const { GridFsStorage } = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+
+
+
+
 require('dotenv').config()
 
 const port = process.env.HOST_PORT || 5000
@@ -22,8 +35,47 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const app = express();
 
-app.use(express.json());
+
+//middleware
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
+// app.use(express.json());
 app.use(handleCorsPolicy);
+
+const conn = mongoose.connection;
+
+let gfs;
+conn.once("open", () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
+//create storage engine
+const storage = new GridFsStorage({
+  url: dbURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({ file: req.file });
+})
+
 
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
