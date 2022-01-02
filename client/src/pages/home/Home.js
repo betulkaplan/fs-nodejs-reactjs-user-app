8baed50fd7ecadd98e2e50cff4fc0f2ad2faeb69
Betul <betulkaplan7@gmail.com>
2022-01-02 22:30:09 +0100

    clear user from home

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -6,11 +6,8 @@ import style from "./home.module.css"
 import ProductCard from "../../components/ProductCard";
 
 function App() {
-  const userSearch = useRef()
   const productSearch = useRef()
-  const [users, setUsers] = useState();
   const [products, setProducts] = useState();
-  const [input, setInput] = useState("");
   const [productInput, setProductInput] = useState("");
   const [isProductModalVisible, setIsProductModalVisible] = useState(false);
   let navigate = useNavigate();
@@ -20,14 +17,7 @@ function App() {
 
   }, [])
 
-  const fetchUsers = async (inp) => {
-    const res = await fetch(`http://localhost:5000/users?name=${inp}`);
-    const response = await res.json();
-    setUsers(response);
-  };
-
   const fetchProducts = async (inp) => {
-    // const res = await fetch(`http://localhost:5000/users?name=${inp}`);
     const res = await fetch(`http://localhost:5000/product?name=${inp}`
       ,
       {
@@ -41,12 +31,7 @@ function App() {
 
   const handleInputChange = (event) => {
     console.log(event.target.id);
-    if (event.target.id === "userSearch") {
-      setInput(event.target.value);
-      if (event.target.value) fetchUsers(event.target.value);
-      else setUsers();
-    }
-    else if (event.target.id === "productSearch") {
+    if (event.target.id === "productSearch") {
       setProductInput(event.target.value);
       if (event.target.value) fetchProducts(event.target.value);
       else setProducts();
@@ -55,20 +40,10 @@ function App() {
 
   };
 
-  const handleDetailClick = async (id) => {
-    console.log(id);
-    navigate(`/detail/${id}`);
-  };
-
-  const addNewUser = () => {
-    navigate("/add");
-  }
-  const addProductUser = () => {
+  const addProduct = () => {
     navigate("/product/add");
   }
-  const showModal = () => {
-    setIsProductModalVisible(true);
-  };
+
 
   const handleOk = () => {
     setIsProductModalVisible(false);
@@ -82,15 +57,6 @@ function App() {
     <div className="App">
       <div className={style.topControl}>
         <h2>User App</h2>
-        {/* <Input
-          ref={userSearch}
-          className={style.topinput}
-          type="text"
-          onChange={handleInputChange}
-          value={input}
-          placeholder="Search User"
-          id="userSearch"
-        /> */}
         <Input
           ref={productSearch}
           className={style.topinput}
@@ -100,15 +66,10 @@ function App() {
           placeholder="Search Product"
           id="productSearch"
         />
-        {/* <Button
-          className={style.topButton}
-          type="primary"
-          onClick={addNewUser}
-        >Add New User</Button> */}
         <Button
           className={style.topButton}
           type="primary"
-          onClick={addProductUser}
+          onClick={addProduct}
         >Add New Product</Button>
         <Modal title="Basic Modal" visible={isProductModalVisible} onOk={handleOk} onCancel={handleCancel}>
           <p>Some contents...</p>
@@ -117,32 +78,7 @@ function App() {
         </Modal>
       </div>
       <div>
-        {(users || products) && <Divider>Result</Divider>}
-        {users &&
-          users.map((user) => (
-            <div
-              key={user._id}
-              style={{
-                display: "flex",
-                justifyContent: "space-around",
-                margin: "10px",
-              }}
-            >
-              <div style={{ lineHeight: "0.3rem" }}>
-                <p style={{ width: "200px" }}>{user.name}</p>
-                <p style={{ width: "200px", fontSize: "0.7rem", color: "gray" }}>@{user.username}</p>
-              </div>
-              <p style={{ width: "200px", fontStyle: "italic" }}>{user.email}</p>
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <Button
-                  type="primary"
-                  onClick={() => handleDetailClick(user._id)}
-                >
-                  Details
-                </Button>
-              </div>
-            </div>
-          ))}
+        {products && <Divider>Result</Divider>}
       </div>
       <div className={style.productsWrapper}>
         {
